0297712462c4cb41e548c232b8ee3229b039ee42
Andrey606 <andreykuluev96@gmail.com>
2020-02-03 15:36:59 +0200

    fix

--- Thread/1/main.cpp
+++ Thread/1/main.cpp
@@ -16,7 +16,7 @@ void DoWork()
     for (size_t i = 0; i < 10; i++)
     {
         cout << "ID потока = " << this_thread::get_id() << "\tDoWork\t" << i << endl;
-        this_thread::sleep_for(chrono::microseconds(1000));
+        this_thread::sleep_for(chrono::milliseconds(1000));
     }
 }
 
@@ -28,15 +28,15 @@ int main()
     cout << "START MAIN" << endl;
 
     // приостанавливает работу текущего потока в (мс)
-    this_thread::sleep_for(chrono::microseconds(5000));
+    //this_thread::sleep_for(chrono::microseconds(5000));
 
     // узнать id текущего потока
-    cout << this_thread::get_id() << endl;
+    //cout << this_thread::get_id() << endl;
 
     // создаем новый поток и передаем в него имя функции (теперь программа выполняеться в двух потокакх)
     thread th(DoWork);
 
-    thread th2(DoWork);
+    //thread th2(DoWork);
 
     // говорим что наш поток может выполняться самостоятельно ни привязываясь не к чему, но после завершения
     // нашего основного потока (main), закончит свою работу и дочерний поток
@@ -45,7 +45,7 @@ int main()
     for (size_t i = 0; i < 10; i++)
     {
         cout << "ID потока = " << this_thread::get_id() << "\tmain\t" << i << endl;
-        this_thread::sleep_for(chrono::microseconds(500));
+        this_thread::sleep_for(chrono::milliseconds(500));
     }
     
     cout << "END MAIN" << endl;
@@ -55,7 +55,7 @@ int main()
     // в тот момент когда мы дожидаемся завершение второго потока, основной будет заблокирован
     th.join();
 
-    th2.join();
+    //th2.join();
    
     return 0;
 }
